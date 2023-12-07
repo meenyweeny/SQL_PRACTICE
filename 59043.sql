f6fd2d4552a828014f31b6ad0ef5237462e41ffb
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-12-07 15:57:11 +0900

    [Lv.3] 있었는데요 없었습니다

---
+++ 59043.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59043
+select ai.animal_id, ai.name
+from animal_ins as ai inner join animal_outs as ao on ai.animal_id = ao.animal_id
+where ai.datetime > ao.datetime
+order by ai.datetime asc
