e0f12b995e6fe41fbcc2077e7ea50d71908a52c7
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2024-03-12 13:59:22 +0900

    Update 276035.sql

--- 276035.sql
+++ 276035.sql
@@ -1,3 +1,4 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/276035
 with fnd as (select * from skillcodes where category like '%Front End%')
 select distinct(dev.id) as id, dev.email as email, dev.first_name as first_name, dev.last_name as last_name
 from developers as dev
