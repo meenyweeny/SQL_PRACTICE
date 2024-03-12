565b60e2541bf7d1b6ed221fe8005bc8da4948ff
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2024-03-12 13:58:44 +0900

    Create 276035.sql

---
+++ 276035.sql
@@ -0,0 +1,6 @@
+with fnd as (select * from skillcodes where category like '%Front End%')
+select distinct(dev.id) as id, dev.email as email, dev.first_name as first_name, dev.last_name as last_name
+from developers as dev
+    inner join fnd as fn
+    on dev.skill_code & fn.code
+order by dev.id asc
