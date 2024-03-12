6931824b41a44074dd82c960861ab4e575fc4dd0
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2024-03-12 10:06:59 +0900

    Update 59042.sql

--- 59042.sql
+++ 59042.sql
@@ -1,5 +1,4 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59042
-select ANIMAL_ID, NAME
-from (select ao.animal_id, ao.name, ai.datetime as indate, ao.datetime as outdate
-      from animal_ins as ai right join animal_outs as ao on ai.animal_id = ao.animal_id) as ar
-      where INDATE is null
+select ao.animal_id as animal_id, ao.name as name
+from animal_ins as ai right join animal_outs as ao on ai.animal_id = ao.animal_id
+where ai.animal_id is null
