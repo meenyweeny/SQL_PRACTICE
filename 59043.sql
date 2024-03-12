df017b3ca034b8d75a518bd893f35c3616a29531
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2024-03-12 10:19:31 +0900

    Update 59043.sql

--- 59043.sql
+++ 59043.sql
@@ -1,5 +1,5 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59043
-select ai.animal_id, ai.name
+select ai.animal_id as animal_id, ai.name as name
 from animal_ins as ai inner join animal_outs as ao on ai.animal_id = ao.animal_id
 where ai.datetime > ao.datetime
 order by ai.datetime asc
